321c96e47677946ea5ffcd6782caca87d1757227
andreamarelli99 <andrea10.marelli@mail.polimi.com>
2025-01-13 19:00:54 +0000

    minor fix

--- run_all_experiments_inference.py
+++ run_all_experiments_inference.py
@@ -19,7 +19,7 @@ from Standard_classifier.inference_cam_generation_Standard_classifier import Std
 
 
 
-os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+os.environ['CUDA_VISIBLE_DEVICES'] = '1'
 
 
 
@@ -28,7 +28,9 @@ config = {
     'architecture': 'resnet50',
     'mode': 'normal',
     'image_size': 512,
-    'tag' : 'GradCAM_seruso_512_no_bg_three_classes_10_epochs_resnet50_batch16_SGD', #'PuzzleFlowCAM_seruso_512_no_bg_three_classes_40_epochs_resnet50_batch16_cl_pcl_re_masking_only_lateral_4_pieces_new_dataset_alpha_2.0_0.0__beta_6.0_0.0',
+    # 'tag' : 'GradCAM_seruso_512_no_bg_three_classes_15_epochs_resnet50_batch32_adam', # 'POFCAM_seruso_512_no_bg_three_classes_40_epochs_resnet50_batch16_cl_pcl_re_masking_only_lateral_4_pieces_new_dataset_alpha_2.0_0.0__beta_6.0_0.0', # GradCAM_seruso_512_no_bg_three_classes_40_epochs_resnet50_batch16_SGD # Puzzle_CAM_seruso_512_no_bg_three_classes_40_epochs_resnet50_batch16_cl_pcl_re_masking_only_lateral_4_pieces_new_dataset_alpha_2.0_0.0
+    # 'tag' : 'Puzzle_CAM_seruso_512_no_bg_three_classes_15_epochs_resnet50_batch32_cl_pcl_re_masking_only_lateral_4_pieces_new_dataset_alpha_2.0_0.0', # 'POFCAM_seruso_512_no_bg_three_classes_40_epochs_resnet50_batch16_cl_pcl_re_masking_only_lateral_4_pieces_new_dataset_alpha_2.0_0.0__beta_6.0_0.0', # GradCAM_seruso_512_no_bg_three_classes_40_epochs_resnet50_batch16_SGD # Puzzle_CAM_seruso_512_no_bg_three_classes_40_epochs_resnet50_batch16_cl_pcl_re_masking_only_lateral_4_pieces_new_dataset_alpha_2.0_0.0
+    'tag' : 'POFCAM_seruso_512_no_bg_three_classes_15_epochs_resnet50_batch32_cl_pcl_re_masking_only_lateral_4_pieces_new_dataset_alpha_2.0_0.0__beta_6.0_0.0', # 'POFCAM_seruso_512_no_bg_three_classes_40_epochs_resnet50_batch16_cl_pcl_re_masking_only_lateral_4_pieces_new_dataset_alpha_2.0_0.0__beta_6.0_0.0', # GradCAM_seruso_512_no_bg_three_classes_40_epochs_resnet50_batch16_SGD # Puzzle_CAM_seruso_512_no_bg_three_classes_40_epochs_resnet50_batch16_cl_pcl_re_masking_only_lateral_4_pieces_new_dataset_alpha_2.0_0.0
     'scales' : '0.2, 0.5, 1.0, 2.0, 4.0, 6.0',
     'imagenet_mean': [0.485, 0.456, 0.406],
     'imagenet_std': [0.229, 0.224, 0.225],
@@ -39,7 +41,7 @@ config = {
 
 root_with_temporal_labels = '../../Datasets/SERUSO_DATASETS/test_set'
 
-batch_size = 16
+batch_size = 64
 image_size = 512
 
 augment = 'colorjitter' #'colorjitter'
@@ -60,12 +62,38 @@ test_transform = transforms.Compose([
 
 test_dataset = seruso_datasets.SerusoTestDataset(img_root = root_with_temporal_labels, classes_subfolders = ['before'], transform= test_transform, with_flow = config['with_flows'], with_mask = config['with_mask'])
 
-std_classifier = Std_classifier_inference(config, test_dataset)
-std_classifier.make_all_cams(False)
+print("\n\nStandard\n\n")
 
-puzzle_cam = Puzzle_CAM_inference(config, test_dataset)
-puzzle_cam.make_all_cams(False)
+folder_path = 'experiments/GradCAM/models/'  # Replace with your folder path
 
+filenames = [os.path.splitext(f)[0] for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
 
-pof_cam = POF_CAM_inference(config, test_dataset)
-pof_cam.make_all_cams()
+for filen in filenames:
+    config['tag'] = filen
+    print(filen)
+    std_classifier = Std_classifier_inference(config, test_dataset, sam_enhance = True)
+    std_classifier.make_all_cams(save_mask = False, visualize = False, norm = False, max_item = 1)
+
+print("\n\nPuzzle\n\n")
+
+folder_path = 'experiments/Puzzle-CAM/models/'  # Replace with your folder path
+
+filenames = [os.path.splitext(f)[0] for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
+
+for filen in filenames:
+    config['tag'] = filen
+    print(filen)
+    puzzle_cam = Puzzle_CAM_inference(config, test_dataset, sam_enhance = True)
+    puzzle_cam.make_all_cams(save_mask = False, visualize = False, norm = False, max_item = 1)
+
+print("\n\nPOF\n\n")
+folder_path = 'experiments/POF-CAM/models/'  # Replace with your folder path
+
+filenames = [os.path.splitext(f)[0] for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
+
+for filen in filenames:
+    config['tag'] = filen
+    print(filen)
+        
+    pof_cam = POF_CAM_inference(config, test_dataset, sam_enhance = True)
+    pof_cam.make_all_cams(save_mask = False, visualize = False, norm = True, max_item = 1)
